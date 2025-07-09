4d7ba070f2b0d72c11dda937b2a1502c210306d9
Sophie Stephenson <38193839+sophiestephenson@users.noreply.github.com>
2025-07-09 11:45:40 -0500

    Enable Overall Consultation Notes (#37)
    * add notes to ConsultationData class
    
    * show notes on homepage
    
    * add working notes form to homepage and save that data
    
    * add overall notes to printout

--- web/view/evidence.py
+++ web/view/evidence.py
@@ -1,56 +1,39 @@
-import json
 import os
-import pickle
 import traceback
 from datetime import datetime
-from enum import Enum
-from operator import itemgetter
 from pprint import pprint
-from time import sleep
 
 from flask import (
     flash,
-    jsonify,
     redirect,
     render_template,
     request,
     send_from_directory,
-    session,
     url_for,
 )
 from flask_bootstrap import Bootstrap
-from wtforms import ValidationError
 
 import config
-from evidence_collection import (  # create_account_summary,; create_app_summary,
-    CONTEXT_PKL_FNAME,
+from evidence_collection import (
     AccountCompromiseForm,
     AccountInvestigation,
-    AppInfo,
     AppInvestigationForm,
     AppSelectPageForm,
-    CheckApps,
     ConsultationData,
     ConsultDataTypes,
+    ConsultNotesData,
     ConsultSetupData,
-    DualUseForm,
+    HomepageNoteForm,
     ManualAddPageForm,
     MultScreenshotEditForm,
-    Pages,
     ScanData,
-    ScanForm,
-    ScreenshotEditForm,
     SetupForm,
-    SpywareForm,
     StartForm,
     TAQData,
     TAQForm,
-    create_overall_summary,
     create_printout,
     get_scan_by_ser,
     get_scan_data,
-    get_scan_obj,
-    get_screenshots,
     get_ser_from_scan_obj,
     get_serial,
     load_json_data,
@@ -61,7 +44,6 @@ from evidence_collection import (  # create_account_summary,; create_app_summary
 )
 from phone_scanner import AndroidScan, IosScan
 from web import app
-from web.view.index import get_device
 
 bootstrap = Bootstrap(app)
 
@@ -114,23 +96,48 @@ def evidence_setup():
 
 
 
-@app.route("/evidence/home", methods={'GET'})
+@app.route("/evidence/home", methods={'GET', 'POST'})
 def evidence_home():
 
-    consult_data = {
-        "setup": load_json_data(ConsultDataTypes.SETUP.value),
-        "taq": load_json_data(ConsultDataTypes.TAQ.value),
-        "scans": load_json_data(ConsultDataTypes.SCANS.value),
-        "accounts": load_json_data(ConsultDataTypes.ACCOUNTS.value)
-    }
+    notes = load_json_data(ConsultDataTypes.NOTES.value)
+    pprint(notes)
+
+    consult_data = ConsultationData(
+        setup=load_json_data(ConsultDataTypes.SETUP.value),
+        taq=load_json_data(ConsultDataTypes.TAQ.value),
+        accounts=load_json_data(ConsultDataTypes.ACCOUNTS.value),
+        scans=load_json_data(ConsultDataTypes.SCANS.value),
+        screenshot_dir = config.SCREENSHOT_LOCATION,
+        notes = load_json_data(ConsultDataTypes.NOTES.value)
+    )
+
+    form = HomepageNoteForm(**consult_data.notes.to_dict())
 
     context = dict(
         task = "evidence-home",
         title=config.TITLE,
-        consultdata=consult_data,
+        consultdata=consult_data.to_dict(),
+        form = form
     )
 
-    return render_template('main.html', **context)
+    if request.method == 'GET':
+
+        return render_template('main.html', **context)
+    
+    if request.method == 'POST':
+
+        pprint(form.data)
+
+        if form.is_submitted() and form.validate():
+
+            new_notes = ConsultNotesData(**form.data)
+            save_data_as_json(new_notes, ConsultDataTypes.NOTES.value)
+
+            return redirect(url_for('evidence_home'))
+
+        elif not form.validate():
+            flash("Form validation error - are you missing required fields?", 'error')
+            return render_template('main.html', **context)
 
 
 @app.route("/evidence/taq", methods={'GET', 'POST'})
@@ -158,9 +165,7 @@ def evidence_taq():
 
     # Submit the form
     if request.method == 'POST':
-        pprint("FORM DATA START")
-        pprint(form.data)
-        pprint("FORM DATA END")
+
         if form.is_submitted() and form.validate():
 
             # load data as class
@@ -652,7 +657,8 @@ def evidence_printout():
         taq=load_json_data(ConsultDataTypes.TAQ.value),
         accounts=load_json_data(ConsultDataTypes.ACCOUNTS.value),
         scans=load_json_data(ConsultDataTypes.SCANS.value),
-        screenshot_dir = config.SCREENSHOT_LOCATION
+        screenshot_dir = config.SCREENSHOT_LOCATION,
+        notes=load_json_data(ConsultDataTypes.NOTES.value)
     )
 
     context = consult_data.to_dict()
